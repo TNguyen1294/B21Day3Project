f7ab80db1541d96d65105f798129a32a218f6874
ThomasThai Q Nguyen <T.Nguyen1294@gmail.com>
2020-11-12 10:45:27 -0500

    Commit 2 Branch Hour1

--- src/B21Day3Project.java
+++ src/B21Day3Project.java
@@ -1,5 +1,7 @@
 public class B21Day3Project {
     public static void main(String[] args) {
         System.out.println("Hello Git last Day");
+        System.out.println("B21 Branch Hour1");
+
     }
 }
